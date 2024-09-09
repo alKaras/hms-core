dfcf7b4285d5a990a452d736b9281c440f0384fe
alex_karas <al.karas.pr@gmail.com>
2024-09-09 23:55:00 +0300

    implemented mail ui verification

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/verification', [\App\Http\Controllers\Api\Auth\AuthController::class, 'showVerificationPage']);
