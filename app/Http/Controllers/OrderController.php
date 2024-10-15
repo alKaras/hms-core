352ededdd0f7671d86b94160f129b2afd2f25aab
alex_karas <al.karas.pr@gmail.com>
2024-10-16 01:54:18 +0300

    refactor filtering orders + added filter by doctorId

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor\Doctor;
+use DB;
 use Exception;
 use Stripe\Stripe;
 use App\Models\Cart;
@@ -247,52 +249,118 @@ class OrderController extends Controller
      */
     public function getOrderByFilter(Request $request)
     {
-        if ($request->session_id !== null) {
-            //Searching by session_id
-            $orderPayment = OrderPayment::where('session_id', $request->session_id)->first();
-            //Get orderInfo 
-            if ($orderPayment) {
-                $order = Order::find($orderPayment->order_id);
-                $orderServices = OrderServices::where('order_id', $order->id)->get();
 
-                return response()->json([
-                    "order" => [
-                        "id" => $order->id,
-                        "sum_total" => $order->sum_total,
-                        "sum_subtotal" => $order->sum_subtotal,
-                        "created_at" => $order->created_at,
-                        "confirmed_at" => $order->confirmed_at,
-                        "status" => $order->status === 2 ? 'SOLD' : ($order->status === 1 ? 'PENDING' : 'CANCELED'),
-                        "reserve_exp" => $order->reserve_exp,
-                        "cancelled_at" => $order->cancelled_at,
-                        "cancel_reason" => $order->cancel_reason,
-                    ],
-                    "order_services" => OrderServiceResource::collection($orderServices),
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'No orders by provided session'
-                ], 404);
-            }
+        if ($request->session_id !== null) {
+            return $this->responseBySessionId($request->session_id);
         } elseif ($request->order_id !== null) {
-            $order = Order::find($request->order_id);
-            if ($order) {
-                return response()->json([
-                    'status' => 'success',
-                    'data' => $order,
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'No orders by provided id'
-                ], 404);
-            }
+            return $this->responseByOrderId($request->order_id);
+        } elseif ($request->doctor_id !== null) {
+            return $this->responseByDoctorId($request->doctor_id);
         } else {
             return response()->json([
                 'status' => 'error',
                 'message' => 'Something Went Wrong'
             ], 500);
+        }
+    }
+
+    /**
+     * Responder for filtering by orderId
+     * @param mixed $order_id
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    private function responseByOrderId($order_id)
+    {
+        $order = Order::find(id: $order_id);
+        if ($order) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $order,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No orders by provided id'
+            ], 404);
+        }
+    }
+
+    /**
+     * Responder for filtering by SessionId
+     * @param mixed $session_id
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    private function responseBySessionId($session_id)
+    {
+        //Searching by session_id
+        $orderPayment = OrderPayment::where(column: 'session_id', operator: $session_id)->first();
+        //Get orderInfo 
+        if ($orderPayment) {
+            $order = Order::find($orderPayment->order_id);
+            $orderServices = OrderServices::where('order_id', $order->id)->get();
+
+            return response()->json([
+                "order" => [
+                    "id" => $order->id,
+                    "sum_total" => $order->sum_total,
+                    "sum_subtotal" => $order->sum_subtotal,
+                    "created_at" => $order->created_at,
+                    "confirmed_at" => $order->confirmed_at,
+                    "status" => $order->status === 2 ? 'SOLD' : ($order->status === 1 ? 'PENDING' : 'CANCELED'),
+                    "reserve_exp" => $order->reserve_exp,
+                    "cancelled_at" => $order->cancelled_at,
+                    "cancel_reason" => $order->cancel_reason,
+                ],
+                "order_services" => OrderServiceResource::collection(resource: $orderServices),
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No orders by provided session'
+            ], 404);
+        }
+    }
+
+    private function responseByDoctorId($doctor_id)
+    {
+        $doctor = Doctor::find($doctor_id);
+        if ($doctor) {
+            $doctorOrders = DB::table('orders as o')
+                ->leftJoin('order_payments as op', 'op.order_id', '=', 'o.id')
+                ->leftJoin('order_payment_logs as opl', 'opl.order_payment_id', '=', 'op.id')
+                ->leftJoin('order_status_ref as osr', 'osr.id', '=', 'o.status')
+                ->leftJoin('order_services as os', 'os.order_id', '=', 'o.id')
+                ->leftJoin('time_slots as ts', 'ts.id', '=', 'os.time_slot_id')
+                ->leftJoin('services as s', 's.id', '=', 'ts.service_id')
+                ->leftJoin('department_content as dc', 'dc.department_id', '=', 's.department_id')
+                ->where('o.status', 2)
+                ->where('ts.doctor_id', $doctor->id)
+                ->groupBy('o.id', 'osr.status_name', 'op.payment_id')
+                ->selectRaw("
+                o.id as orderId,
+                osr.status_name as paidStatus,
+                JSON_ARRAYAGG(JSON_OBJECT('serviceName', s.name, 'departmentTitle', dc.title, 'startTime', ts.start_time)) as services,
+                op.payment_id as paymentId
+            ")
+                ->get();
+
+            return response()->json([
+                'status' => 'success',
+                'data' => collect($doctorOrders)->map(function ($order) {
+                    return [
+                        'id' => $order->orderId,
+                        'paid_status' => $order->paidStatus,
+                        'payment_id' => $order->paymentId,
+                        'serviceData' => json_decode($order->services, true),
+                    ];
+                })
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => "No data for provided doctor_id #{$doctor_id}"
+            ], 404);
         }
     }
 }
