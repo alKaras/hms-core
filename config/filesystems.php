1d7758ef038ef83c68bd782cf47dac55a39846a0
alex_karas <al.karas.pr@gmail.com>
2024-11-21 23:49:56 +0200

    created backup config + backup scheduler for every saturdays at 2 am

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,11 +35,15 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
+        'backupsStorage' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backups'),
+        ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
